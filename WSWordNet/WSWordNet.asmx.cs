4c5140dab48a81be42195ee96c25eeb6c6213b15
manzamb <manzamb@hotmail.com>
2019-03-15 19:15:38 -0500

    Arreglado Wordnet

--- WSWordNet/WSWordNet.asmx.cs
+++ WSWordNet/WSWordNet.asmx.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Services;
-using WSWordNet.Logic;
-using WSWordNet.DataAccess;
-
+using Syn.WordNet;
 
 namespace WSWordNet
 {
 
-    //Syn.WordNet.WordNetEngine wordNetEngine = new Syn.WordNet.WordNetEngine();
-    //Syn.WordNet.PartOfSpeech pos = new Syn.WordNet.PartOfSpeech();
-    //Syn.WordNet.SynSet ss = new Syn.WordNet.SynSet(pos, 0, wordNetEngine);
-    //Console.WriteLine("words form wordnet" + ss.Words);
+
 
     /// <summary>
     /// Summary description for Service1
@@ -27,47 +19,34 @@ namespace WSWordNet
     {
 
         [WebMethod]
-        public string[] GetConceptsOfQuery(string query)
-        {
-            string path = HttpContext.Current.Server.MapPath("./");
-            string newPath = path + @"\bin\dict\";
-            WordNetManager manager = new WordNetManager(newPath);
-            Concept concepto = manager.GetConceptOfQuery(query);
-            string[] words = concepto.GetAllNounSynSets();
-            return words;
-        }
-
-        [WebMethod]
         public string GetSynonym(string concept)
         {
+            string sinonimos = "";
             string conceptSynonym = "";
-            string Synonym = "";
-            int mayor = int.MinValue;
-            edu.smu.tspell.wordnet.NounSynset nounSynset;
-            string path = HttpContext.Current.Server.MapPath("./");
-            string newPath = path + @"\bin\dict\";
-            java.lang.System.setProperty("wordnet.database.dir", newPath);
-            edu.smu.tspell.wordnet.WordNetDatabase database = edu.smu.tspell.wordnet.WordNetDatabase.getFileInstance();
-            //se obtienen los sinonimos del concepto concept
-            edu.smu.tspell.wordnet.Synset[] synsets = database.getSynsets(concept, edu.smu.tspell.wordnet.SynsetType.NOUN);
 
-            if (synsets.Length != 0)
-            {   //se guarda los elementos del concepto
-                nounSynset = (edu.smu.tspell.wordnet.NounSynset)(synsets[0]);
-                foreach (string wf in nounSynset.getWordForms())
-                {
-                    //valor que indica el uso del sinonimo
-                    int pos = nounSynset.getTagCount(wf);
-                    //se obtiene el sinonimo de mayor uso
-                    if (pos > mayor && wf != concept)
-                    {
-                        mayor = pos;
-                        Synonym = wf;
-                    }
-                }
-                if (Synonym != "")
-                    conceptSynonym = "(" + concept + " + " + Synonym + ")";
+            //Cargar la base de datos de WordNet
+            WordNetEngine wordNetEngine = new WordNetEngine();
+            PartOfSpeech pos = new PartOfSpeech();
+
+            //var directory = Directory.GetCurrentDirectory();
+            var directory = HttpContext.Current.Server.MapPath("./") + "bin\\WordNet";
+            wordNetEngine.LoadFromDirectory(directory);
+
+
+            //Utilizar la librería para traer los sinonimos
+            var synSetList = wordNetEngine.GetSynSets(concept);
+            if (synSetList.Count == 0) return "(" + concept + " + )";
+
+            foreach (var synSet in synSetList)
+            {
+                var words = string.Join(" + ", synSet.Words);
+
+                sinonimos = sinonimos + words;
             }
+
+            conceptSynonym = "(" + sinonimos + ")";
+
+
             return conceptSynonym;
         }   
     }
